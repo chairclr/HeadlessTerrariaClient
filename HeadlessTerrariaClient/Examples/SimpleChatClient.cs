4f001f79e1f8cd5992e475b42f0e8b53f3f2f5e7
Project ArkAngel <projectaa76@gmail.com>
2022-04-15 17:10:19 -0400

    make more code async and simplify boilerplate

--- HeadlessTerrariaClient/Examples/SimpleChatClient.cs
+++ HeadlessTerrariaClient/Examples/SimpleChatClient.cs
@@ -1,53 +1,51 @@
 ﻿using System;
+using System.Threading.Tasks;
 using HeadlessTerrariaClient;
 using HeadlessTerrariaClient.Terraria;
 using HeadlessTerrariaClient.Terraria.ID;
 using HeadlessTerrariaClient.Terraria.Chat;
 using HeadlessTerrariaClient.Client;
 using HeadlessTerrariaClient.Util;
+using System.Threading;
 
 namespace HeadlessTerrariaClient.Examples
 {
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleChatClient chatClient = new SimpleChatClient();
+            chatClient.Start().Wait();
+        }
+    }
     public class SimpleChatClient
     {
         const string ServerIP = "127.0.0.1";
         const int ServerPort = 7777;
-        static void Main(string[] args)
+        
+
+        public async Task Start()
         {
-            Start();
-
-            System.Threading.Thread.Sleep(-1);
-        }
-
-        public static void Start()
-        {
-            // Create an empty world
-            ClientWorld clientWorld = new ClientWorld();
-
             // Create a new client
             HeadlessClient HeadlessClient = new HeadlessClient();
 
             // Random client UUID
-            HeadlessClient.clientUUID = Guid.NewGuid().ToString();
+            HeadlessClient.SetRandomUUID();
 
             // Assaign world reference
-            HeadlessClient.World = clientWorld;
+            HeadlessClient.World = new ClientWorld();
 
             // Name the player
-            HeadlessClient.PlayerFile.name = $"ExampleChatClient";  
+            HeadlessClient.PlayerFile.name = $"ExampleChatClient";
 
-            // Softcore player
-            HeadlessClient.PlayerFile.difficulty = PlayerDifficultyID.SoftCore;
-
-            // Load default player style so we arent some weird white goblin
-            HeadlessClient.PlayerFile.LoadDefaultAppearence();
-            HeadlessClient.PlayerFile.LoadDefaultInventory();
+            // Softcore player, Defualt inventory, Default appearence 
+            HeadlessClient.PlayerFile.LoadDefaultPlayer();
 
             // This can bypass some anti-cheats that attempt to block headless clients
             HeadlessClient.Settings.AutoSyncPlayerZone = true;
 
             // Run code when a chat message is recived
-            HeadlessClient.ChatMessageRecieved += (HeadlessClient client, ChatMessage message) =>
+            HeadlessClient.ChatMessageRecieved += async (HeadlessClient client, ChatMessage message) =>
             {
                 // Messages of id 255 are not from another player
                 if (message.author != 255)
@@ -62,7 +60,10 @@ namespace HeadlessTerrariaClient.Examples
             };
 
             // Connect to a server
-            HeadlessClient.Connect(ServerIP, ServerPort);
+            await HeadlessClient.Connect(ServerIP, ServerPort);
+
+            // Stop the console from closing
+            await Task.Delay(Timeout.Infinite);
         }
 
     }
--- HeadlessTerrariaClient/Terraria/Player.cs
+++ HeadlessTerrariaClient/Terraria/Player.cs
@@ -183,5 +183,11 @@ namespace HeadlessTerrariaClient.Terraria
             inventory[1] = new Item(ItemID.CopperPickaxe);
             inventory[2] = new Item(ItemID.CopperAxe);
         }
+        public void LoadDefaultPlayer()
+        {
+            LoadDefaultAppearence();
+            LoadDefaultInventory();
+            difficulty = PlayerDifficultyID.SoftCore;
+        }
     }
 }
